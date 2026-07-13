df1dc3f82fa55ae09d553bb799158f58d3099d62
Jitendra Prajapati <jitendra.pra40@gmail.com>
2026-07-13 12:26:46 +0530

    myhourly architecture

---
+++ src/main/java/com/my_hourly/MyHourlyApplication.java
@@ -0,0 +1,13 @@
+package com.my_hourly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHourlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHourlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_hourly/MyHourlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_hourly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHourlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
