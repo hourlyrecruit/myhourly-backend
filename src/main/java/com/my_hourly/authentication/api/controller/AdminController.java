2fc766d01e75cb00a0ef469369ee51ec854dc9fa
Jitendra Prajapati <jitendra.pra40@gmail.com>
2026-07-17 22:52:50 +0530

    fixed file and reporting manager issue solved

--- src/main/java/com/my_hourly/authentication/api/controller/AdminController.java
+++ src/main/java/com/my_hourly/authentication/api/controller/AdminController.java
@@ -23,7 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('SUPER_ADMIN', 'MANAGER', HR)")
+@PreAuthorize("hasAnyRole('SUPER_ADMIN', 'MANAGER', 'HR')")
 @Tag(name = "Admin Controller", description = "Only SUPER_ADMIN and MANAGER can access these endpoint.")
 public class AdminController {
 
